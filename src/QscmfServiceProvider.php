a4ec1a359b309ff12027650ab1dc6864e393703f
tider <tider@quansitech.com>
2018-11-01 18:08:48 +0800

    t

--- src/QscmfServiceProvider.php
+++ src/QscmfServiceProvider.php
@@ -2,8 +2,19 @@
 namespace QSCMF;
 
 use Illuminate\Support\ServiceProvider;
+use QSCMF\Commands\InstallCommand;
 
 class QscmfServiceProvider extends ServiceProvider{
+
+    public function register(){
+        if($this->app->runningInConsole()){
+            $this->commands(InstallCommand::class);
+        }
+    }
+
+    public function boot(){
+        
+    }
 
     
 }
