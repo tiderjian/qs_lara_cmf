818fd43843a49111a5583aa319c4bf83328f8cbb
tider <tider@quansitech.com>
2018-11-16 17:02:08 +0800

    t

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -27,8 +27,24 @@ class InstallCommand extends Command{
      *
      * @return void
      */
-    public function handle(FilesSystem $filesystem){
+    public function handle(Filesystem $filesystem){
+        $this->info("Attempting to set locale to zh_CN");
+        if ($filesystem->exists(config_path('app.php'))) {
+            $str = $filesystem->get(config_path('app.php'));
+
+            if ($str !== false) {
+                $str = str_replace("'locale' => 'en'", "'locale' => 'zh_CN'", $str);
+
+                $filesystem->put(config_path('app.php'), $str);
+            }
+        } else {
+            $this->warn('Unable to locate "config/app.php".  Did you move this file?');
+            $this->warn('You will need to update this manually.  Change "locale" to "zh_CN" in your app.php');
+        }
+        $this->getLaravel()['config']->set('app.locale', 'zh_CN');
+
         $this->call('voyager:install');
-        $this->call('voyager:admin admin@email.com', ['--create']);
+
+        $this->call('voyager:admin', ['email' => 'admin@email.com', '--create' => true]);
     }
 }
